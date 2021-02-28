40c8271bc0412f03803337732b93bcbc5a77b1bb
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2021-02-28 12:53:33 +0000

    Grumphp can now run the tests using PDO SQLite

--- database_settings.php
+++ database_settings.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 $dbHost = 'database';  // database for Docker (set in docker-compose)
 //       'localhost';  // localhost on LINUX
@@ -8,9 +9,15 @@ $dbUser = 'root';    // Your DB user name
 $dbPassword = '';    // Your DB password
 
 $dbCharset = "utf8mb4";
+
 $dbOptions = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 ];
+
+if (getenv('APP_ENV') && getenv('APP_ENV') !== 'TESTING') {
+    $dbOptions += [
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    ];
+}
